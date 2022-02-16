1444cee4624ac0146372fc2c0394559f96899664
Osel <osellim@gmail.com>
2022-02-16 19:39:25 +0800

    experiment with component stye, adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from "react";
 import logo from './logo.svg';
-import pp from './assets/Profile Pic.jpg'
+import pp from './assets/Profile Pic.jpg';
+import Navbar from './components/Navbar/Navbar.js';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <img src={pp} className="Profile-pic" alt="pp" />
-        <p>
+        <p className="text-3xl font-bold underline">
           Work In Progress
         </p>
         <a
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+import React from "react";
+
+function Navbar() {
+  
+    return (
+      <div className="Navbar">
+        This is the navbar
+      </div>
+    );
+  }
+  
+  export default Navbar;
