d89c51b764fddbd1b371192a58f493044fa4bd9f
Osel <osellim@gmail.com>
2022-02-18 22:53:26 +0800

    build error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import pp from './assets/Profile Pic.jpg';
 import './App.css';
 
