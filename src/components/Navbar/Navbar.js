0b93df0c059e4e8351c62a73a99196340648bbdf
Osel <osellim@gmail.com>
2022-04-06 00:01:01 +0800

    adding toggle for navBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,11 @@
+import { React, useState } from 'react';
 
 function Navbar() {
-
+  var [menuOpen, toggleMenu] = useState(false)
+  const toggler = () => {
+    menuOpen ? toggleMenu(false) : toggleMenu(true)
+    console.log("toggleMenu", menuOpen)
+  }
   return (
     <nav className=" bg-teal-500 p-6">
       <div className="wrapper flex items-center justify-between flex-wrap max-w-6xl m-auto">
@@ -8,12 +13,14 @@ function Navbar() {
           <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
           <span className="font-semibold text-xl tracking-tight">Osel</span>
         </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <div className="block lg:hidden" >
+          {menuOpen ? <button onClick={toggler} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-          </button>
+          </button> : <button onClick={toggler} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><title>Close</title><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" /></svg>
+          </button>}
         </div>
-        <div className="w-full block lg:flex lg:items-center lg:w-auto">
+        {menuOpen && <div className="w-full block lg:flex lg:items-center lg:w-auto transition-transform">
           <div className="text-sm lg:flex-grow">
             <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
               Home
@@ -24,14 +31,14 @@ function Navbar() {
             <a href="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
               About
             </a>
-            <a href="/resume" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            <a href="/resume" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 lg:mr-0">
               Resume
             </a>
           </div>
           {/* <div>
           <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
         </div> */}
-        </div>
+        </div>}
       </div>
     </nav>
   );
