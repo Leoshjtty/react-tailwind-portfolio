617908d69a8206d02410aaa372e616d59e33eed2
Osel <osellim@gmail.com>
2022-04-06 15:23:29 +0800

    fix missing nav when lg

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><title>Close</title><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" /></svg>
           </button>}
         </div>
-        {menuOpen && <div className="w-full block lg:flex lg:items-center lg:w-auto transition-transform">
+        {menuOpen && <div className="lg:hidden w-full block lg:flex lg:items-center lg:w-auto transition-transform">
           <div className="text-sm lg:flex-grow">
             <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
               Home
@@ -35,10 +35,23 @@ function Navbar() {
               Resume
             </a>
           </div>
-          {/* <div>
-          <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-        </div> */}
         </div>}
+        <div className="hidden lg:block w-full block lg:flex lg:items-center lg:w-auto transition-transform">
+          <div className="text-sm lg:flex-grow">
+            <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Home
+            </a>
+            <a href="/projects" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Projects
+            </a>
+            <a href="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              About
+            </a>
+            <a href="/resume" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 lg:mr-0">
+              Resume
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
