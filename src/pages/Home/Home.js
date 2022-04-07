e6b4e2419de6b3bd97acfea096e84268faa85213
Osel <osellim@gmail.com>
2022-04-07 12:06:13 +0800

    adding margin btm

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import logo from '../../logo.svg';
 function Home() {
   const filterColor = "brightness(0) invert(65%) sepia(66%) saturate(2615%) hue-rotate(128deg) brightness(90%) contrast(84%)"
   return (
-    <div className="mt-8">
+    <div className="my-8">
       <p>Home </p>
       <img src={logo} className="App-logo" alt="logo" style={{filter: filterColor}}/>
       <img src={logo} className="App-logo" alt="logo" style={{animation: "App-logo-spin infinite 4s linear", filter: filterColor}}/>
