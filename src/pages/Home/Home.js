9bcf5b74777e4486c11fc75f9f02ab7c713269c1
Osel <osellim@gmail.com>
2022-04-07 15:02:57 +0800

    fix scrollbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,13 @@ import logo from '../../logo.svg';
 function Home() {
   const filterColor = "brightness(0) invert(65%) sepia(66%) saturate(2615%) hue-rotate(128deg) brightness(90%) contrast(84%)"
   return (
-    <div className="my-8">
+    <div className="mt-8 mb-20">
       <p>Home </p>
       <img src={logo} className="App-logo" alt="logo" style={{filter: filterColor}}/>
       <img src={logo} className="App-logo" alt="logo" style={{animation: "App-logo-spin infinite 4s linear", filter: filterColor}}/>
       <img src={logo} className="App-logo" alt="logo" style={{animation: "App-logo-spin infinite 2s linear",filter: filterColor}}/>
       <img src={logo} className="App-logo" alt="logo" style={{animation: "App-logo-spin infinite 1s linear", filter: filterColor}}/>
-      <img src={logo} className="App-logo" alt="logo" style={{animation: "App-logo-spin infinite 0.2s linear",filter: filterColor}}/>
+      <img src={logo} className="App-logo mb-20" alt="logo" style={{animation: "App-logo-spin infinite 0.2s linear",filter: filterColor}}/>
         <p>
           Work In Progress
         </p>
